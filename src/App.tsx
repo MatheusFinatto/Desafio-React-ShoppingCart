12f9f1cee64dd54bab2730a4eabfe945768b847e
MatheusFinatto <matheusfinatto2@gmail.com>
2023-05-25 08:53:43 -0300

    feat: header and mockups

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route, Outlet } from "react-router-dom";
+
+import Payment from "./pages/Payment";
+import Confirmation from "./pages/Confirmation";
+import Navbar from "./components/Navbar";
+import Bag from "./pages/Bag";
+import Summary from "./components/Summary";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState<any>([]);
+
+  const getProducts = async (): Promise<void> => {
+    const res = await fetch(
+      "https://run.mocky.io/v3/d6e9a93f-9741-4494-b81e-637a8e9b8ddd"
+    );
+    const data = await res.json();
+    setData(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route path="sacola" element={<Bag data={data} />} />
+          <Route path="confirmacao" element={<Confirmation />} />
+          <Route path="pagamento" element={<Payment />} />
+        </Route>
+      </Routes>
+      <Outlet />
+      <Summary data={data} />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,72 @@
+import { NavLink, Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+type LinkProps = {
+  className?: any;
+  children: React.ReactNode;
+  to: string;
+  activeStyle?: any;
+};
+
+const Link = ({ className, children, to }: LinkProps): JSX.Element => (
+  <NavLink className={className} to={to}>
+    {children}
+  </NavLink>
+);
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #878787;
+  padding: 10px;
+  border-bottom: 1px solid #878787;
+  box-sizing: border-box;
+  &.active {
+    box-shadow: 0px -10px 0px -6px black inset;
+    color: #000;
+  }
+`;
+
+const List = styled.ul`
+  display: flex;
+  width: 100%;
+  list-style: none;
+  justify-content: center;
+  padding: 8px;
+  align-items: center;
+`;
+
+const Navbar = () => {
+  return (
+    <nav>
+      <List>
+        <li>
+          <StyledLink
+            to="/sacola"
+            className={({ isActive }: any) => (isActive ? "active" : "")}
+          >
+            Sacola
+          </StyledLink>
+        </li>
+        <li>
+          <StyledLink
+            to="/pagamento"
+            className={({ isActive }: any) => (isActive ? "active" : "")}
+          >
+            Pagamento
+          </StyledLink>
+        </li>
+        <li>
+          <StyledLink
+            to="/confirmacao"
+            className={({ isActive }: any) => (isActive ? "active" : "")}
+          >
+            Confirmação
+          </StyledLink>
+        </li>
+      </List>
+      <Outlet />
+    </nav>
+  );
+};
+
+export default Navbar;
